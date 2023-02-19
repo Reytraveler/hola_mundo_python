b2e6e4556f227db5517c0f21b7e64fa55b7f1fd9
Reytraveler <125778625+Reytraveler@users.noreply.github.com>
2023-02-18 23:59:56 -0300

    Add files via upload

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,27 +1,11 @@
-#Primer ejemplo de la función print
-#en la clase N1
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Inove Coding School
+# Version: 2.0
 
-print("Hola mundo")
-print('Hola mundo desde VSC')
-print("Clase Na:1")
-print("Clase Na:", 1)   
-print('Hola Clase')
-
-print("El resultado de sumar 8 y 9 es:", 8+9)
-print("El resultado de sumar", 8, "y", 9, "es:", 8+9)
-print(f"el resultado de sumar {8} y {9} es {8+9}")
+# Descripcion:
+# Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
 
 
-# print("Hola")
-# print("Hola")
-# print("Hola")
-# print("Hola")
-# print("Hola")
-
-print("Hola\n"*5)
-
-""" Esto me 
-permite
-comentar 
-varias 
-lineas"""
+print("Hola Mundo!")
